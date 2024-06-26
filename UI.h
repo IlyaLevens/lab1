7c43517b067a0357be3624bc89ce6ee25c41a230
IlyaLevens <75679804+IlyaLevens@users.noreply.github.com>
2024-06-26 09:40:55 +0300

    Add files via upload

---
+++ UI.h
@@ -0,0 +1,10 @@
+#ifndef LABORATORY1_MENU_H
+#define LABORATORY1_MENU_H
+
+// displayUI - функция, которая выводит меню.
+void displayUI();
+
+// yourchoice - функция, которая определяет ход работы программы.
+void yourchoice();
+
+#endif //LABORATORY1_MENU_H
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "UI.h"
+#include "DynamicArray.h"
+
+// Динамический массив
+// Целые числа, строки
+// Сортировка, map, where, Конкатенация
+
+int main(){
+    int ch;
+    yourchoice();
+
+}
